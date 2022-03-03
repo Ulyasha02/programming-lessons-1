dccc974482ad6b34af9a76318888b1b3b39efcbc
Ulyasha02 <ulshelyakova02@icloud.com>
2022-03-03 13:34:39 +0300

    добавила систему логирования запросов к серверу

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const http = require("http");
 const requestHandler = (request, response) => {
-response.end("Hello! It is my first server.");
-};
+    response.end("Hello! It is my first server.");
+    };
+const fs = require("fs");
+fs.appendFileSync("hello.txt", "Новое сообщение в файле!");
+fs.appendFile("hello.txt", "Привет МИР!", function(error){
+    if(error) throw error; // если возникла ошибка
+    console.log("Запись файла завершена. Содержимое файла:");
+    let data = fs.readFileSync("hello.txt", "utf8");
+    console.log(data); // выводим считанные данные
+});
 http.createServer(requestHandler).listen(13000);
