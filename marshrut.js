0499431d6ad322b2c63d4dfde050d2c131ab7ef0
Ulyasha02 <ulshelyakova02@icloud.com>
2022-03-03 12:44:51 +0300

    добавила маршрутизацию

---
+++ marshrut.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const requestHandler = (request, response) => {
+response.setHeader("Content-Type", "text/html; charset=utf-8;");
+if(request.url === "/home" || request.url === "/"){
+response.write("<h2>Home</h2>");
+}
+else if(request.url == "/first"){
+response.write("<h2>first</h2>");
+}
+else if(request.url == "/contact"){
+response.write("<h2>Contacts</h2>");
+}
+else{
+response.write("<h2>Not found</h2>");
+}
+response.end();
+};
+http.createServer(requestHandler).listen(13000);
